712600fd8d2daa084d1ff1cfac9b0335a580f2fd
Kacper Szot <kacperszot1@gmail.com>
2020-01-06 18:22:13 +0100

    Initial commit

---
+++ src/main/java/io/novschola/NovscholaApplication.java
@@ -0,0 +1,13 @@
+package io.novschola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovscholaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovscholaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/novschola/NovscholaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.novschola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovscholaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
