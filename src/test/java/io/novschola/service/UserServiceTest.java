37b997ed2d583283eec2280de4925747aa1b0a46
Kacper Szot <kacperszot1@gmail.com>
2020-01-25 22:43:27 +0100

    test for user update method

--- src/test/java/io/novschola/service/UserServiceTest.java
+++ src/test/java/io/novschola/service/UserServiceTest.java
@@ -1,21 +1,27 @@
 package io.novschola.service;
 
+import io.novschola.exception.BadRequestException;
+import io.novschola.exception.ItemNotFoundException;
 import io.novschola.model.User;
 import io.novschola.repositories.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.Assert;
-import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class UserServiceTest {
     private static final String EMAIL = "test@test.com";
-    private static final String NAME = "John" ;
+    private static final String NAME = "John";
     private static final String LASTNAME = "Doo";
     private static final String PASSWORD = "PASSWORD";
     @Mock
@@ -23,6 +29,7 @@ class UserServiceTest {
     BCryptPasswordEncoder bCryptPasswordEncoder;
     UserService userService;
     User user;
+
     @BeforeEach
     void setUp() {
         bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
@@ -37,12 +44,29 @@ class UserServiceTest {
 
     @Test
     void create() {
-    when(userRepository.save(any())).thenReturn(user);
-    when(userRepository.save(any())).thenReturn(user);
-    User newUSer = userService.create(this.user);
-    verify(userRepository, times(1)).save(any());
-    Assert.notNull(newUSer.getActivationKey(),"activation key is null");
-    assertThat(newUSer.getPassword(), not(equalTo(PASSWORD)));
+        when(userRepository.save(any())).thenReturn(user);
+        User newUSer = userService.create(this.user);
+        verify(userRepository, times(1)).save(any());
+        Assert.notNull(newUSer.getActivationKey(), "activation key is null");
+        assertThat(newUSer.getPassword(), not(equalTo(PASSWORD)));
+    }
+
+    @Test
+    void updateTest() throws Exception{
+        final String NEWLASTNAME = "New Last Name";
+        final Long ID = 2L;
+        when(userRepository.save(any())).thenReturn(user);
+        user.setId(ID);
+        user.setLastName(NEWLASTNAME);
+        user = userService.update(this.user);
+        verify(userRepository, times(1)).save(user);
+        assertEquals(user.getLastName(), NEWLASTNAME);
+        try {
+            user.setId(null);
+            userService.update(user);
+        } catch (BadRequestException e) {
+            Assertions.assertThat(e).isInstanceOf(BadRequestException.class);
+        }
     }
 
 }
