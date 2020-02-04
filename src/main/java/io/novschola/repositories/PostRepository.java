da416fec965c8ebb8af8b54cf9a0299a4dc1819d
Kacper Szot <kacperszot1@gmail.com>
2020-02-04 21:52:25 +0100

    changed repository to use pagination

--- src/main/java/io/novschola/repositories/PostRepository.java
+++ src/main/java/io/novschola/repositories/PostRepository.java
@@ -2,13 +2,14 @@ package io.novschola.repositories;
 
 import io.novschola.model.Post;
 import io.novschola.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository  extends CrudRepository<Post, Long> {
-    Iterable<Post> findAllByTitleLike(String title);
-    Iterable<Post> findAllByContentLike(String content);
-    Iterable<Post> findAllByAuthor(User author);
-    Iterable<Post> findAllByAuthor_Id(Long authorId);
+public interface PostRepository  extends PagingAndSortingRepository<Post, Long> {
+    Iterable<Post> findAllByTitleLike(String title, Pageable pageable);
+    Iterable<Post> findAllByContentLike(String content, Pageable pageable);
+    Iterable<Post> findAllByAuthor(User author, Pageable pageable);
+    Iterable<Post> findAllByAuthor_Id(Long authorId, Pageable pageable);
 }
