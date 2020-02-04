61f560cc13b73eb11b80b55932b9d5f6fcbf6872
Kacper Szot <kacperszot1@gmail.com>
2020-02-04 22:33:43 +0100

    typo fix

--- src/main/java/io/novschola/repositories/PostRepository.java
+++ src/main/java/io/novschola/repositories/PostRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository  extends PagingAndSortingRepository<Post, Long> {
-    Page<Post> findAllByContentLikeOrContentLike(String query, Pageable pageable);
+    Page<Post> findAllByContentContainingOrTitleContaining(String content, String title, Pageable pageable);
     Page<Post> findAllByAuthor(User author, Pageable pageable);
     Page<Post> findAllByAuthor_Id(Long authorId, Pageable pageable);
 }
