9191a05f8c8c46b00785ab774ab34644074ccc29
Kacper Szot <kacperszot1@gmail.com>
2020-02-04 22:21:54 +0100

    find all by author id

--- src/main/java/io/novschola/service/PostService.java
+++ src/main/java/io/novschola/service/PostService.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class PostService {
     private PostRepository postRepository;
@@ -23,11 +21,11 @@ public class PostService {
     }
 
     public Page<Post> findAllByAuthor(User author, Pageable pageable) {
-        return postRepository.findAllByAuthor(author,pageable);
+        return postRepository.findAllByAuthor(author, pageable);
     }
 
-    public Page<Post> findAllByAuthorId(Long Id, Pageable pageable) {
-        return null;
+    public Page<Post> findAllByAuthorId(Long id, Pageable pageable) {
+        return postRepository.findAllByAuthor_Id(id, pageable);
     }
 
     public Page<Post> search(String query, Pageable pageable) {
