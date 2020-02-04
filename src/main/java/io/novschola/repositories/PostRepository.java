88104d83e4d9ede5a71b173623496482d0d87dad
Kacper Szot <kacperszot1@gmail.com>
2020-02-04 22:24:34 +0100

    better search repository method

--- src/main/java/io/novschola/repositories/PostRepository.java
+++ src/main/java/io/novschola/repositories/PostRepository.java
@@ -9,8 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository  extends PagingAndSortingRepository<Post, Long> {
-    Page<Post> findAllByTitleLike(String title, Pageable pageable);
-    Page<Post> findAllByContentLike(String content, Pageable pageable);
+    Page<Post> findAllByContentLikeOrContentLike(String query, Pageable pageable);
     Page<Post> findAllByAuthor(User author, Pageable pageable);
     Page<Post> findAllByAuthor_Id(Long authorId, Pageable pageable);
 }
