553fad5387381b919b6aad39682d7983fbc66a5b
Kacper Szot <kacperszot1@gmail.com>
2020-02-22 21:40:27 +0100

    swagger config class

--- src/main/java/io/novschola/config/SecurityConfig.java
+++ src/main/java/io/novschola/config/SecurityConfig.java
@@ -55,7 +55,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().httpBasic().and()
                 .authorizeRequests()
-                .antMatchers("/v1/auth")
+                .antMatchers("/v1/auth",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/v1/api-docs",
+                        "/webjars/**")
                 .permitAll().anyRequest().authenticated().and().addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class).userDetailsService(jwtUserDetailsService);
         http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
     }
