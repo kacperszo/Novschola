4fbd025747c7aaf497030f164cb16d1afae9fdc7
Kacper Szot <kacperszot1@gmail.com>
2020-02-22 21:41:11 +0100

    changed bad credentials exception response code to 401

--- src/main/java/io/novschola/exception/BadCredentialsException.java
+++ src/main/java/io/novschola/exception/BadCredentialsException.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception class invoked when provided credentials are incorrect
- * returns http code 403
+ * returns http code 401
  * @author Kacper Szot
  */
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class BadCredentialsException extends RuntimeException{
 }
