3a02d4a946268758e058e3f8870e9837d600bf32
Kacper Szot <kacperszot1@gmail.com>
2020-02-29 19:26:20 +0100

    create a dto representing the user update request

---
+++ src/main/java/io/novschola/api/v1/model/dto/request/UpdateUserRequest.java
@@ -0,0 +1,22 @@
+package io.novschola.api.v1.model.dto.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+/**
+ * The class represents a single request made to update user
+ * @author Kacper Szot
+ */
+@Data
+public class UpdateUserRequest {
+    @NotBlank(message = "firstName cannot not be blank")
+    @Size(max = 50, message = "firstName length must be lower then 50")
+    String firstName;
+    @NotBlank(message = "lastName cannot be blank")
+    @Size(max = 50, message = "lastName length must be lower then 50")
+    String lastName;
+    @NotNull(message = "bio cannot be blank")
+    String bio;
+}
