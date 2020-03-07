7e1e4fb8335e7b68b2d3e36699f0964ae4a3e33e
Kacper Szot <kacperszot1@gmail.com>
2020-03-07 16:24:56 +0100

    add test class for post controller

---
+++ src/test/java/io/novschola/api/v1/controller/PostControllerTest.java
@@ -0,0 +1,37 @@
+package io.novschola.api.v1.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPosts() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void searchPosts() {
+    }
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void testUpdatePost() {
+    }
+}
