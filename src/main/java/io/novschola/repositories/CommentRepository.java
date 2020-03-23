72e1eea370e758109981812c611f4d602a2dd9ac
Kacper Szot <kacperszot1@gmail.com>
2020-03-23 16:53:43 +0100

    add findAllByAuthor to the comment repository

--- src/main/java/io/novschola/repositories/CommentRepository.java
+++ src/main/java/io/novschola/repositories/CommentRepository.java
@@ -1,6 +1,7 @@
 package io.novschola.repositories;
 
 import io.novschola.model.Comment;
+import io.novschola.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,5 +14,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByPostId(Long id);
 
     Page<Comment> findAllByAuthorId(Long id);
+
+    Page<Comment> findAllByAuthor(User author);
 
 }
